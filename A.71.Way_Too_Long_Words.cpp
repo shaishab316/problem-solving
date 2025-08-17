cea61170857f8bbc92d4b4918389fcefb92aadd3
shaishabcoding <shaishabchandrashil@gmail.com>
2025-08-17 12:49:29 +0600

    feat: Add solved Codeforces problems (A.4, A.71, A.158, A.231, A.282)
    This commit adds solutions for the following Codeforces problems:
    
    - A.4. Watermelon
    - A.71. Way Too Long Words
    - A.158. Next Round
    - A.231. Team
    - A.282. Bit++
    
    Each file contains the problem statement as a comment block at the end of the file.

--- A.71.Way_Too_Long_Words.cpp
+++ A.71.Way_Too_Long_Words.cpp
@@ -27,8 +27,6 @@ int main()
 A. Way Too Long Words
 https://codeforces.com/problemset/problem/71/A
 
-time limit per test1 second
-memory limit per test256 megabytes
 Sometimes some words like "localization" or "internationalization" are so long that writing them many times in one text is quite tiresome.
 
 Let's consider a word too long, if its length is strictly more than 10 characters. All too long words should be replaced with a special abbreviation.
