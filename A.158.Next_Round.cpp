c0b4d18bc35be94b64d48570a082158484db82ba
shaishabcoding <shaishabchandrashil@gmail.com>
2025-08-17 12:39:10 +0600

    Add: Solution for Codeforces problem 158A (Next Round)

---
+++ A.158.Next_Round.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl '\n'
+#define ll long long
+
+int main()
+{
+  int n, k;
+  cin >> n >> k;
+
+  vector<int> scores(n);
+  for (int i = 0; i < n; i++)
+    cin >> scores[i];
+
+  int threshold = scores[k - 1];
+  int count = 0;
+
+  for (int score : scores)
+  {
+    if (score >= threshold && score > 0)
+    {
+      count++;
+    }
+  }
+
+  cout << count << endl;
+
+  return 0;
+}
