8faa5f7cf4946c1af4d846b7b10583fdb62a95be
shaishab316 <shaishabchandrashil@gmail.com>
2025-09-21 09:32:17 +0600

    feat: Add solution for LeetCode problem 19 - Remove Nth Node From End of List
    This commit adds a C++ solution for LeetCode problem 19, \"Remove Nth Node From End of List\". The solution uses the two-pointer approach (fast and slow pointers) to achieve a single-pass removal of the nth node from the end of the linked list. A dummy node is used to handle edge cases like removing the head node.

---
+++ 19.remove-nth-node-from-end-of-list.cpp
@@ -0,0 +1,97 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+// @lc code=start
+class Solution
+{
+public:
+    ListNode *removeNthFromEnd(ListNode *head, int n)
+    {
+        // Create dummy node to handle edge cases (like removing head)
+        ListNode *dummy = new ListNode(NULL);
+        dummy->next = head;
+
+        ListNode *fast = dummy, *slow = dummy;
+
+        for (int i = 0; i < n; i++)
+        {
+            fast = fast->next;
+        }
+
+        while (fast->next != NULL)
+        {
+            fast = fast->next;
+            slow = slow->next;
+        }
+
+        // delete N-th node
+        slow->next = slow->next->next;
+
+        return dummy->next;
+    }
+};
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=19 lang=cpp
+ *
+ * [19] Remove Nth Node From End of List
+ *
+ * https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
+ *
+ * algorithms
+ * Medium (49.68%)
+ * Likes:    20525
+ * Dislikes: 873
+ * Total Accepted:    3.7M
+ * Total Submissions: 7.5M
+ * Testcase Example:  '[1,2,3,4,5]\n2'
+ *
+ * Given the head of a linked list, remove the n^th node from the end of the
+ * list and return its head.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: head = [1,2,3,4,5], n = 2
+ * Output: [1,2,3,5]
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: head = [1], n = 1
+ * Output: []
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: head = [1,2], n = 1
+ * Output: [1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in the list is sz.
+ * 1 <= sz <= 30
+ * 0 <= Node.val <= 100
+ * 1 <= n <= sz
+ *
+ *
+ *
+ * Follow up: Could you do this in one pass?
+ *
+ */
