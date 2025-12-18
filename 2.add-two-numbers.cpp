2dbaf4ac0e3ddcc1c0455d28dbea9ba782953e2b
shaishab316 <shaishabchandrashil@gmail.com>
2025-12-18 15:03:05 +0600

    feat: Add solution for LeetCode problem 2 - Add Two Numbers

---
+++ 2.add-two-numbers.cpp
@@ -0,0 +1,93 @@
+struct ListNode {
+  int val;
+  ListNode* next;
+  ListNode() : val(0), next(nullptr) {}
+  ListNode(int x) : val(x), next(nullptr) {}
+  ListNode(int x, ListNode* next) : val(x), next(next) {}
+};
+
+// @lc code=start
+class Solution {
+ public:
+  ListNode* addTwoNumbers(ListNode* l1, ListNode* l2, int curry = 0) {
+    if (!l1 && !l2 && !curry) {
+      return nullptr;
+    }
+
+    int sum = curry;
+
+    if (l1) {
+      sum += l1->val;
+      l1 = l1->next;
+    }
+
+    if (l2) {
+      sum += l2->val;
+      l2 = l2->next;
+    }
+
+    ListNode* newNode = new ListNode(sum % 10);
+    newNode->next = addTwoNumbers(l1, l2, sum / 10);
+
+    return newNode;
+  }
+};
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=2 lang=cpp
+ *
+ * [2] Add Two Numbers
+ *
+ * https://leetcode.com/problems/add-two-numbers/description/
+ *
+ * algorithms
+ * Medium (46.81%)
+ * Likes:    35608
+ * Dislikes: 7081
+ * Total Accepted:    6.5M
+ * Total Submissions: 13.7M
+ * Testcase Example:  '[2,4,3]\n[5,6,4]'
+ *
+ * You are given two non-empty linked lists representing two non-negative
+ * integers. The digits are stored in reverse order, and each of their nodes
+ * contains a single digit. Add the two numbers and return the sum as a linked
+ * list.
+ *
+ * You may assume the two numbers do not contain any leading zero, except the
+ * number 0 itself.
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: l1 = [2,4,3], l2 = [5,6,4]
+ * Output: [7,0,8]
+ * Explanation: 342 + 465 = 807.
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: l1 = [0], l2 = [0]
+ * Output: [0]
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: l1 = [9,9,9,9,9,9,9], l2 = [9,9,9,9]
+ * Output: [8,9,9,9,0,0,0,1]
+ *
+ *
+ *
+ * Constraints:
+ *
+ *
+ * The number of nodes in each linked list is in the range [1, 100].
+ * 0 <= Node.val <= 9
+ * It is guaranteed that the list represents a number that does not have
+ * leading zeros.
+ *
+ *
+ */
