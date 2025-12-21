5349dacf2991fc74bc1d3efeb6a26b42842d87f8
shaishab316 <shaishabchandrashil@gmail.com>
2025-12-21 12:41:43 +0600

    feat: Add solution for LeetCode problem 1 - Two Sum

---
+++ 1.two-sum.java
@@ -0,0 +1,80 @@
+import java.util.*;
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        HashMap<Integer, Integer> mp = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int comp = target - nums[i];
+            if (mp.containsKey(comp)) {
+                return new int[] { (int) mp.get(comp), i };
+            }
+            mp.put(nums[i], i);
+        }
+
+        return new int[] { -1, -1 };
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=1 lang=java
+ *
+ * [1] Two Sum
+ *
+ * https://leetcode.com/problems/two-sum/description/
+ *
+ * algorithms
+ * Easy (56.70%)
+ * Likes: 66120
+ * Dislikes: 2460
+ * Total Accepted: 19.9M
+ * Total Submissions: 35.1M
+ * Testcase Example: '[2,7,11,15]\n9'
+ *
+ * Given an array of integers nums and an integer target, return indices of the
+ * two numbers such that they add up to target.
+ * 
+ * You may assume that each input would have exactly one solution, and you may
+ * not use the same element twice.
+ * 
+ * You can return the answer in any order.
+ * 
+ * 
+ * Example 1:
+ * 
+ * 
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+ * 
+ * 
+ * Example 2:
+ * 
+ * 
+ * Input: nums = [3,2,4], target = 6
+ * Output: [1,2]
+ * 
+ * 
+ * Example 3:
+ * 
+ * 
+ * Input: nums = [3,3], target = 6
+ * Output: [0,1]
+ * 
+ * 
+ * 
+ * Constraints:
+ * 
+ * 
+ * 2 <= nums.length <= 10^4
+ * -10^9 <= nums[i] <= 10^9
+ * -10^9 <= target <= 10^9
+ * Only one valid answer exists.
+ * 
+ * 
+ * 
+ * Follow-up: Can you come up with an algorithm that is less than O(n^2) time
+ * complexity?
+ */
