ee9349cf3f107305da0cb72bbef8f5f22710d516
shaishab316 <shaishabchandrashil@gmail.com>
2025-12-22 18:12:29 +0600

    feat: Optimize solution for LeetCode problem 977 - Squares of a Sorted Array using two-pointer technique

--- 977.squares-of-a-sorted-array.java
+++ 977.squares-of-a-sorted-array.java
@@ -1,15 +1,25 @@
-import java.util.*;
-
 // @lc code=start
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] *= nums[i];
+        int n = nums.length;
+        int[] result = new int[n];
+
+        int left = 0, right = n - 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            int leftSQ = nums[left] * nums[left];
+            int rightSQ = nums[right] * nums[right];
+
+            if (leftSQ > rightSQ) {
+                result[i] = leftSQ;
+                left++;
+            } else {
+                result[i] = rightSQ;
+                right--;
+            }
         }
 
-        Arrays.sort(nums);
-
-        return nums;
+        return result;
     }
 }
 // @lc code=end
@@ -19,3 +29,15 @@ class Solution {
  *
  * [977] Squares of a Sorted Array
  */
+
+// class Solution {
+// public int[] sortedSquares(int[] nums) {
+// for (int i = 0; i < nums.length; i++) {
+// nums[i] *= nums[i];
+// }
+
+// Arrays.sort(nums);
+
+// return nums;
+// }
+// }
