7f425d6f6a178f366085367cc9ba48c5abdccc65
shaishab316 <shaishabchandrashil@gmail.com>
2025-12-22 16:07:47 +0600

    feat: Refactor solution for LeetCode problem 77 - Combinations to use bit manipulation

--- 77.combinations.java
+++ 77.combinations.java
@@ -3,26 +3,26 @@ import java.util.*;
 // @lc code=start
 class Solution {
     public List<List<Integer>> combine(int n, int k) {
-        List<List<Integer>> result = new ArrayList<>();
+        List<List<Integer>> subsets = new ArrayList<>();
+        int subsetsCount = 1 << n;
 
-        helper(n, k, result, new ArrayList<>(), 1);
+        for (int mask = 0; mask < subsetsCount; mask++) {
+            if (Integer.bitCount(mask) != k) {
+                continue; // insuficent mask
+            }
 
-        return result;
-    }
+            List<Integer> subset = new ArrayList<>();
 
-    private void helper(int n, int k, List<List<Integer>> result, List<Integer> current, int start) {
-        if (current.size() == k) {
-            result.add(new ArrayList<>(current));
-            return; // break
+            for (int i = 0; i < n; i++) {
+                if (((mask >> i) & 1) == 1) {
+                    subset.add(i + 1);
+                }
+            }
+
+            subsets.add(subset);
         }
 
-        for (int i = start; i <= n; i++) {
-            current.add(i);
-
-            helper(n, k, result, current, i + 1);
-
-            current.remove(current.size() - 1); // backtrack
-        }
+        return subsets;
     }
 }
 // @lc code=end
@@ -32,3 +32,31 @@ class Solution {
  *
  * [77] Combinations
  */
+
+// ! using Backtracking
+
+// class Solution {
+// public List<List<Integer>> combine(int n, int k) {
+// List<List<Integer>> result = new ArrayList<>();
+
+// helper(n, k, result, new ArrayList<>(), 1);
+
+// return result;
+// }
+
+// private void helper(int n, int k, List<List<Integer>> result, List<Integer>
+// current, int start) {
+// if (current.size() == k) {
+// result.add(new ArrayList<>(current));
+// return; // break
+// }
+
+// for (int i = start; i <= n; i++) {
+// current.add(i);
+
+// helper(n, k, result, current, i + 1);
+
+// current.remove(current.size() - 1); // backtrack
+// }
+// }
+// }
