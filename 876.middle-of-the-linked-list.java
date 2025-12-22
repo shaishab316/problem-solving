509533a107fe54914a2bd37732881a73488f35cb
shaishab316 <shaishabchandrashil@gmail.com>
2025-12-22 13:20:38 +0600

    feat: Add solution for LeetCode problem 876 - Middle of the Linked List

---
+++ 876.middle-of-the-linked-list.java
@@ -0,0 +1,38 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=876 lang=java
+ *
+ * [876] Middle of the Linked List
+ */
