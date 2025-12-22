3cf3cd0a6ecc3c1a2ab58401aadb7c466ade55cd
shaishab316 <shaishabchandrashil@gmail.com>
2025-12-22 13:25:23 +0600

    feat: Add solution for LeetCode problem 206 - Reverse Linked List

---
+++ 206.reverse-linked-list.java
@@ -0,0 +1,39 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=206 lang=java
+ *
+ * [206] Reverse Linked List
+ */
