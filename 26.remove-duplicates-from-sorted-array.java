3d4b0e0d4aea0e922bb4ae721084a69cced558ab
Shaishab Chandra Shil <shaishabchandrashil@gmail.com>
2026-03-07 20:26:04 +0600

    feat: Optimize solution for LeetCode problem 26 - Remove Duplicates from Sorted Array by eliminating temporary storage

--- 26.remove-duplicates-from-sorted-array.java
+++ 26.remove-duplicates-from-sorted-array.java
@@ -1,21 +1,17 @@
-import java.util.*;
-
 // @lc code=start
 class Solution {
     public int removeDuplicates(int[] nums) {
-        List<Integer> temp = new ArrayList<>();
+        int slow = 0;
 
-        for (int num : nums) {
-            if (temp.isEmpty() || temp.getLast() != num) {
-                temp.add(num);
+        for (int fast = 1; fast < nums.length; fast++) {
+            if (nums[slow] != nums[fast]) {
+                slow++; // Note: slow always one way late so we need to move
+
+                nums[slow] = nums[fast];
             }
         }
 
-        for (int i = 0; i < temp.size(); i++) {
-            nums[i] = temp.get(i);
-        }
-
-        return temp.size();
+        return slow + 1; // Note: slow always one way late so we need to move
     }
 }
 // @lc code=end
@@ -24,4 +20,25 @@ class Solution {
  * @lc app=leetcode id=26 lang=java
  *
  * [26] Remove Duplicates from Sorted Array
+ */
+
+// Solution 1:
+/*
+ * class Solution {
+ * public int removeDuplicates(int[] nums) {
+ * List<Integer> temp = new ArrayList<>();
+ * 
+ * for (int num : nums) {
+ * if (temp.isEmpty() || temp.getLast() != num) {
+ * temp.add(num);
+ * }
+ * }
+ * 
+ * for (int i = 0; i < temp.size(); i++) {
+ * nums[i] = temp.get(i);
+ * }
+ * 
+ * return temp.size();
+ * }
+ * }
  */
