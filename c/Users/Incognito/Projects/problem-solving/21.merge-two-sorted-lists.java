ea3c8b92854feaa935568caa0c385ac2b2e57536
Shaishab Chandra Shil <shaishabchandrashil@gmail.com>
2026-03-10 21:10:24 +0600

    feat: Implement solution for LeetCode problem 21 - Merge Two Sorted Lists

---
+++ c/Users/Incognito/Projects/problem-solving/21.merge-two-sorted-lists.java
@@ -0,0 +1,52 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode(0);
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                curr.next = list1;
+                list1 = list1.next; // move
+            } else {
+                curr.next = list2;
+                list2 = list2.next; // move
+            }
+            curr = curr.next; // move
+        }
+
+        // if have extra
+        if (list1 != null) {
+            curr.next = list1;
+        }
+        if (list2 != null) {
+            curr.next = list2;
+        }
+
+        return dummy.next; // peek curr
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=21 lang=java
+ *
+ * [21] Merge Two Sorted Lists
+ */
