90c0acf53e497f757f95ada6998d20ef50f1dfcf
Shaishab Chandra Shil <shaishabchandrashil@gmail.com>
2026-03-19 20:13:46 +0600

    feat: Implement solution for LeetCode problem 226 - Invert Binary Tree

---
+++ c/Users/Incognito/Projects/problem-solving/226.invert-binary-tree.java
@@ -0,0 +1,41 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+
+        TreeNode left = root.left;
+
+        root.left = invertTree(root.right);
+        root.right = invertTree(left);
+
+        return root;
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=226 lang=java
+ *
+ * [226] Invert Binary Tree
+ */
