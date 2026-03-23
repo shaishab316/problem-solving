77ad700c7d88fe9324b8958a765753bff50bc089
Shaishab Chandra Shil <shaishabchandrashil@gmail.com>
2026-03-23 20:56:41 +0600

    feat: Implement solution for LeetCode problem 110 - Balanced Binary Tree

---
+++ c/Users/Incognito/Projects/problem-solving/110.balanced-binary-tree.java
@@ -0,0 +1,45 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return Math.abs(getHeight(root.left) - getHeight(root.right)) <= 1 &&
+                isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    private int getHeight(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        return 1 + Math.max(getHeight(node.left), getHeight(node.right));
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=110 lang=java
+ *
+ * [110] Balanced Binary Tree
+ */
