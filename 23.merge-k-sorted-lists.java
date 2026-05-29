a30ace49f6c2acc3b465bb7b88786259240ea5c8
Shaishab Chandra Shil <shaishabchandrashil@gmail.com>
2026-05-29 12:31:54 +0600

    feat: Implement solution for LeetCode problem 23 - Merge k Sorted Lists

---
+++ 23.merge-k-sorted-lists.java
@@ -0,0 +1,53 @@
+import java.util.PriorityQueue;
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> heap = new PriorityQueue<>((a, b) -> a.val - b.val);
+
+        for (ListNode node : lists) {
+            if (node != null) {
+                heap.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+
+        while (!heap.isEmpty()) {
+            ListNode node = heap.poll();
+            current.next = node;
+            current = current.next;
+
+            if (node.next != null) {
+                heap.offer(node.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=23 lang=java
+ *
+ * [23] Merge k Sorted Lists
+ */
