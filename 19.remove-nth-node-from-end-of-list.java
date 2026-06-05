83ab1cdd53fa662d3d6e5e307f13031f71ba4505
Shaishab Chandra Shil <shaishabchandrashil@gmail.com>
2026-06-05 23:22:26 +0600

    feat: Implement two-pointer solution for LeetCode problem 19 - Remove Nth Node From End of List

---
+++ 19.remove-nth-node-from-end-of-list.java
@@ -0,0 +1,50 @@
+
+public class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// @lc code=start
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+
+        while (n > 0) {
+            fast = fast.next;
+
+            n--;
+        }
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        slow.next = slow.next.next;
+
+        return dummy.next;
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=19 lang=java
+ *
+ * [19] Remove Nth Node From End of List
+ */
