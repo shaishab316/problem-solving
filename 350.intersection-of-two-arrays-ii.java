ecbbd057889eafa753056c3a24679a1dc8c80a04
shaishab316 <shaishabchandrashil@gmail.com>
2026-06-23 17:07:36 +0600

    feat: Implement two-pointer with sorting solution for LeetCode problem 350 - Intersection of Two Arrays II

---
+++ 350.intersection-of-two-arrays-ii.java
@@ -0,0 +1,48 @@
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+// @lc code=start
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int n = nums1.length;
+        int m = nums2.length;
+
+        List<Integer> intersection = new ArrayList<>();
+
+        int i = 0, j = 0;
+
+        while (i < n && j < m) {
+            if (nums1[i] > nums2[j]) {
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+            } else {
+                intersection.add(nums1[i]);
+
+                i++;
+                j++;
+            }
+        }
+
+        int[] result = new int[intersection.size()];
+        int idx = 0;
+
+        for (int num : intersection) {
+            result[idx++] = num;
+        }
+
+        return result;
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=350 lang=java
+ *
+ * [350] Intersection of Two Arrays II
+ */
