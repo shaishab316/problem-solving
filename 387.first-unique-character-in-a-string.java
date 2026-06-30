1ae015c9c0a6ae96035fa075df327ee373593d19
Shaishab Chandra Shil <shaishabchandrashil@gmail.com>
2026-06-30 18:53:05 +0600

    feat: Add frequency array solution for LeetCode problem 387 - First Unique Character in a String

---
+++ 387.first-unique-character-in-a-string.java
@@ -0,0 +1,26 @@
+
+// @lc code=start
+class Solution {
+    public int firstUniqChar(String s) {
+        int[] freq = new int[26];
+
+        for (char ch : s.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
+// @lc code=end
+
+/*
+ * @lc app=leetcode id=387 lang=java
+ *
+ * [387] First Unique Character in a String
+ */
